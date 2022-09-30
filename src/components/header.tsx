b864921e59f138c4de8426ba2fbb81eb1986b84e
Sofya Howard <showard@evolveip.net>
2022-09-30 16:37:32 -0400

    feat: add a styled component example

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,16 @@
+import styled from 'styled-components';
+
 const Header = () => {
-  return <header>Header</header>;
+  return <HeaderStyles>Header</HeaderStyles>;
 };
+
+/** Example of Styled component in use
+ *
+ * _Delete if you are not using it_
+ */
+const HeaderStyles = styled.header`
+  margin: 0;
+  padding: 0;
+`;
 
 export default Header;
